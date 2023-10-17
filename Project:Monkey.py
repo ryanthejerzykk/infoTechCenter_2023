33b8e5f620226b310ac348bb27e9ff596e5dc752
jerzyk <ryanjerzyk@gmail.com>
2023-10-17 08:30:46 -0400

    add description and welcome print

---
+++ Project:Monkey.py
@@ -0,0 +1,8 @@
+"""
+Our Welcome Screen will start our program letting
+drivers know that the InfoTech Center 2023 is loading
+"""
+
+print("\nWelcome to the Infotech Center 2023")
+
+print("InfoTech Center 2023 is loading...")
