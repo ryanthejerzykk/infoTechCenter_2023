ea111cd634394d46a15c0471002316dc23f51a1d
jerzyk <ryanjerzyk@gmail.com>
2023-10-23 08:23:22 -0400

    added time and veriebal to time and stable (he made me add that part)

--- Project:Monkey.py
+++ Project:Monkey.py
@@ -2,7 +2,11 @@
 Our Welcome Screen will start our program letting
 drivers know that the InfoTech Center 2023 is loading
 """
+#import libraries here
+import time
 
-print("\nWelcome to the Infotech Center 2023")
+time_to_sleep = 2
 
+print("\nWelcome to the InfoTech Center 2023")
+time.sleep(time_to_sleep)
 print("InfoTech Center 2023 is loading...")
