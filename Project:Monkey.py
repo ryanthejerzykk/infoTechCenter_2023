b77b8378ba1de4b255568b0072fe3862f27f7996
jerzyk <ryanjerzyk@gmail.com>
2023-10-25 07:38:42 -0400

    added a timer to info tech center loading

--- Project:Monkey.py
+++ Project:Monkey.py
@@ -9,4 +9,12 @@ time_to_sleep = 2
 
 print("\nWelcome to the InfoTech Center 2023")
 time.sleep(time_to_sleep)
+
+#add code to have elipes add a dot ever.5 seconds
+x = 0
+y = 0
+
+while x != 20:
+
+
 print("InfoTech Center 2023 is loading...")
