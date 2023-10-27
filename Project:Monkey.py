0dbb349038e1d25a5a5c663604f7d775d8e1b429
jerzyk <ryanjerzyk@gmail.com>
2023-10-27 08:39:10 -0400

    imported library's and defined a gasLevelgauge

---
+++ Project:Monkey.py
@@ -0,0 +1,16 @@
+print("*****************************************************")
+print("gasoline branch\n")
+
+# import the library here
+import random
+from time import sleep
+
+def gasLevelGauge():
+    gasLevelList = ["Empty", " Low", "Quarter Tank", "Half tank", "Three Quarter Tank", "Full Tank"]
+    currenGasLevel = random.choice (gasLevelList)
+    return currenGasLevel
+
+
+
+
+
