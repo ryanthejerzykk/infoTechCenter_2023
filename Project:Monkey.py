702f0b051af3e5b18eef3c7704698d284e71fcc0
jerzyk <ryanjerzyk@gmail.com>
2023-10-30 08:17:23 -0400

    added gaslevelalert function and added how many mile till next gastation

--- Project:Monkey.py
+++ Project:Monkey.py
@@ -4,13 +4,17 @@ print("gasoline branch\n")
 # import the library here
 import random
 from time import sleep
-
+# function that list gas levels and chooses one, and returning its value
 def gasLevelGauge():
     gasLevelList = ["Empty", " Low", "Quarter Tank", "Half tank", "Three Quarter Tank", "Full Tank"]
     currenGasLevel = random.choice (gasLevelList)
     return currenGasLevel
 
 
+# function will read the gas level gauge and say a close gas station if empty
+def gasLevelAlert():
+    milesToGasStation = round(random.uniform(1,25),1)
+    print(milesToGasStation)
 
 
-
+gasLevelAlert()
