f649474dd7e8797361cc820fd7995a1551862147
jerzyk <ryanjerzyk@gmail.com>
2023-11-01 07:58:19 -0400

    added gas list of gas stations

--- Project:Monkey.py
+++ Project:Monkey.py
@@ -7,15 +7,21 @@ from time import sleep
 # function that list gas levels and chooses one, and returning its value
 def gasLevelGauge():
     gasLevelList = ["Empty", " Low", "Quarter Tank", "Half tank", "Three Quarter Tank", "Full Tank"]
-    currenGasLevel = random.choice (gasLevelList)
-    return currenGasLevel
+    currentGasLevel = random.choice (gasLevelList)
+    return currentGasLevel
 
+# Function with list of gasStations
+def listofgasStation():
+    gasStation = ["shell","Speedway","Exon","Costco","Maraton","BP","Circle K","Wesco"]
+    gasStationsNear =random.choice(gasStation)
+    return gasStationsNear
 
-# function will read the gas level gauge and say a close gas station if empty
+# function will read the ags level gauge and say a close gas station if empty
 def gasLevelAlert():
-    milesToGasStationLow = round(random.uniform(1,25),1)
-    milesToGasStationQuarter = round(random.uniform(25.1,50),1)
-    print("low =",milesToGasStationLow)
+    milesToGasStationLow = round(random.uniform(1, 25), 1)
+    milesToGasStationQuarter = round(random.uniform(25.1, 50), 1)
+
+    print("low =", milesToGasStationLow)
     print("Quarter=", milesToGasStationQuarter)
 
 
